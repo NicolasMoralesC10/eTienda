a6e5ce575a6214d9c93c19625900088832228f62
Morales <nicolaschompi@gmail.com>
2025-02-17 11:20:41 -0500

    incorporacion Usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -21,6 +21,70 @@ const listartodos = async (req, res) => {
   }
 };
 
+const nuevo = async (req, res) => {
+  // llega el objeto en el boddy del request
+  let datos = {
+    nombre: req.body.nombre,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    telefono: req.body.telefono,
+    esAdmin: req.body.esAdmin,
+    direccion: req.body.direccion,
+    zip: req.body.zip,
+    ciudad: req.body.ciudad,
+    pais: req.body.pais,
+  };
+  try {
+    // instancia del modelo producto (collection)
+    const usuariosNuevo = new usuarios(datos);
+    usuariosNuevo.save(); // * Este metodo guarda en el mongo
+
+    return res.send({
+      estado: true,
+      mensaje: "insercion exitosa",
+    });
+  } catch (error) {
+    return res.send({
+      estado: false,
+      mensaje: `ha ocurrido un error en la consulta ${error}`,
+    });
+  }
+};
+
+// actualizar por id del usuarios
+const actualizarxid = async (req, res) => {
+  // recibe el parametro de la consulta
+  let id = req.params.id;
+  // payload que viene en en lody :: los datos que manda el formulario
+  let datos = {
+    nombre: req.body.nombre,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    telefono: req.body.telefono,
+    esAdmin: req.body.esAdmin,
+    direccion: req.body.direccion,
+    zip: req.body.zip,
+    ciudad: req.body.ciudad,
+    pais: req.body.pais,
+  };
+
+  try {
+    let consulta = await usuarios.findByIdAndUpdate(id, datos).exec();
+
+    return res.send({
+      estado: true,
+      mensaje: "actualizacion exitosa",
+    });
+  } catch (error) {
+    return res.send({
+      estado: false,
+      mensaje: `error en la actualizacion: ${error}`,
+    });
+  }
+};
+
 module.exports = {
   listartodos,
+  nuevo,
+  actualizarxid,
 };
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,12 +6,12 @@ const router = express.Router();
 // instanciamos el controlador correspondiente
 const usuarioCtr = require("../controllers/usuarios");
 
-// rutas que entregara el modulo productos
+// * rutas que entregara el modulo productos
 
 router.get("/usuarios/listartodos", usuarioCtr.listartodos);
-/* router.post("/usuario/nuevo", usuarioCtr.nuevo);
-router.get("/usuario/buscarxid/:id", usuarioCtr.buscarxid);
-router.delete("/usuario/borrarxid/:id", usuarioCtr.borrarxid);
-router.put("/usuario/actualizarxid/:id", usuarioCtr.actualizarxid); */
+router.post("/usuarios/nuevo", usuarioCtr.nuevo);
+/* router.get("/usuarios/buscarxid/:id", usuarioCtr.buscarxid);
+router.delete("/usuarios/borrarxid/:id", usuarioCtr.borrarxid); */
+router.put("/usuarios/actualizarxid/:id", usuarioCtr.actualizarxid);
 // .....
 module.exports = router;
